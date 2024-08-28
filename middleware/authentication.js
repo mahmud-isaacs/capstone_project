55aa8a3aecff0883afc7fb4fb493381e87cbe18f
Mahmud.isaacs03 <Mahmud.isaacs03@gmail.com>
2024-08-28 15:00:02 +0200

    fixed login

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,33 +1,38 @@
 import { compare } from "bcrypt";
 import { loginUserDb } from "../model/usersDB.js";
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
-dotenv.config()
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const checkUser = async(req,res,next)=>{
-    const {userAdd,userPass} = req.body
-    let hashedPassword = (await loginUserDb(userAdd)).userPass
-    
-    console.log(hashedPassword);
+const checkUser = async (req, res, next) => {
+    const { userName, userPass } = req.body;
 
     try {
+        const user = await loginUserDb(userName);
+        
+        if (!user) {
+            return res.status(401).send('User does not exist');
+        }
+
+        const hashedPassword = user.userPass;
+
         compare(userPass, hashedPassword, (err, result) => {
             if (err) {
                 return res.status(500).send('Internal server error');
             }
-            
+
             if (result) {
-                let token = jwt.sign({ userAdd: userAdd }, process.env.SECRET_KEY, { expiresIn: '1h' });
+                const token = jwt.sign({ userName: userName }, process.env.SECRET_KEY, { expiresIn: '1h' });
                 console.log(token);
                 req.body.token = token;
                 next();
-                return;
             } else {
-                res.status(401).send('Your password is incorrect');
+                return res.status(401).send('Your password is incorrect');
             }
         });
     } catch (error) {
-        res.status(500).send('An unexpected error occurred');
+        return res.status(500).send('An unexpected error occurred');
     }
-}
-export {checkUser}
+};
+
+export { checkUser };
