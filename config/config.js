82232db0fb2cf7e77e5ea7ac71d6a8883f071211
Mahmud.isaacs03 <Mahmud.isaacs03@gmail.com>
2024-08-28 08:21:15 +0200

    Present

---
+++ config/config.js
@@ -0,0 +1,13 @@
+import { createPool } from "mysql2/promise"
+import { config } from "dotenv"
+
+config
+
+const pool = createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD
+})
+
+export default pool
